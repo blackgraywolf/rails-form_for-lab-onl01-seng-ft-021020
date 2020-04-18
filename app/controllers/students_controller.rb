29afeb2436ce24fd4a31bf7276ca259dd0443b4d
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-18 20:29:15 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,33 +2,33 @@
 class StudentsController < ApplicationController
   
   def index
-  @students =  Student.all
+  @students1 =  Student.all
   end
   
    def new
     
-    @student = Student.new
+    @student2 = Student.new
     
     end
     
    def create
     # binding.pry
-      @student = Student.create(params.require(:student).permit(:first_name, :last_name))
-        redirect_to student_path(@student)
+      @student3 = Student.create(params.require(:student).permit(:first_name, :last_name))
+        redirect_to student_path(@student3)
      end
       
     def show
-        @student = Student.find(params[:id])
+        @student4 = Student.find(params[:id])
     end 
 
     def edit
-        @student = Student.find(params[:id])
+        @student5 = Student.find(params[:id])
     end 
         
      def update
-        @student = Student.find(params[:id])
-        @student.update(params.require(:student).permit(:first_name, :last_name))
-        redirect_to student_path(@student)
+        @student6 = Student.find(params[:id])
+        @student6.update(params.require(:student).permit(:first_name, :last_name))
+        redirect_to student_path(@student6)
     end 
     
 end

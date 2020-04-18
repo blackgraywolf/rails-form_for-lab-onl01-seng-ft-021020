0edfa45e9976188f8ec35be73911df6a296345e1
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-04-18 20:37:14 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 2) do
   create_table "school_classes", force: :cascade do |t|
     t.string   "title"
     t.integer  "room_number"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "students", force: :cascade do |t|
